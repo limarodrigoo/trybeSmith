fcaa6d4fc05ef656238e07f783fa186bdb5cef91
Rodrigo Lima <limarodrigoo@outlook.com>
2022-03-29 15:43:12 -0300

    :bento: add user validation

--- src/middlewares/Validation.ts
+++ src/middlewares/Validation.ts
@@ -1,8 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
-import productSchema from '../schemas/productSchema';
+import { productSchema, userSchema } from '../schemas';
 
 export default class Validation {
-  public productNameValidate = (req: Request, res: Response, next: NextFunction) => {
+  public productValidate = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
     try {
       const { name, amount } = req.body;
       const { error } = productSchema.validate({ name, amount });
@@ -17,4 +21,22 @@ export default class Validation {
       return res.status(500).json({ message: 'Algo deu errado' });
     }
   };
-} 
+
+  public userValidate = (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { username, classe, level, password } = req.body;
+      const { error } = userSchema.validate(
+        { username, classe, level, password },
+        { convert: false },
+      );
+      if (error) {
+        const [code, message] = error.details[0].message.split('|');
+        return res.status(Number(code)).json({ error: message });
+      }
+      next();
+    } catch (e) {
+      console.log(e);
+      return res.status(500).json({ message: 'Algo deu errado' });
+    }
+  };
+}
