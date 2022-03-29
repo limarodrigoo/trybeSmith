c57d7e931a92fee74e987472e36af77ee1aa53b8
Rodrigo Lima <limarodrigoo@outlook.com>
2022-03-29 16:57:49 -0300

    :bento: implement order end point

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { ProductRouter, UserRouter } from './router';
+import { OrderRouter, ProductRouter, UserRouter } from './router';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', ProductRouter);
 app.use('/users', UserRouter);
+app.use('/orders', OrderRouter);
 
 export default app;
