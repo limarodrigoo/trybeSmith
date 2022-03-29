87eb98e700a9fc8a80e7469dc5635a880c6685e1
Rodrigo Lima <limarodrigoo@outlook.com>
2022-03-29 16:56:00 -0300

    :bento: add order router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
 import ProductRouter from './ProductRouter';
 import UserRouter from './UserRouter';
+import OrderRouter from './OrderRouter';
 
 export {
   ProductRouter,
   UserRouter,
+  OrderRouter,
 };
