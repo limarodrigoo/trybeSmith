1306a8ae55a365cc77f10edf9df88b034e4da172
Rodrigo Lima <limarodrigoo@outlook.com>
2022-03-30 15:44:22 -0300

    🚚 rename error class

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction, Errback } from 'express';
 
-export default class Validation {
+export default class Error {
   public productNameValidate = (e: Errback, req: Request, res: Response, _next: NextFunction) => {
     console.log(e);
     return res.status(500).json({ message: 'Algo deu errado' });
