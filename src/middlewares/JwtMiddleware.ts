ca7faf8b27f0c329b1c60f8083087dd192db5f3f
Rodrigo Lima <limarodrigoo@outlook.com>
2022-03-30 15:46:24 -0300

    🍱 create token validation middleware

--- src/middlewares/JwtMiddleware.ts
+++ src/middlewares/JwtMiddleware.ts
@@ -1,22 +1,45 @@
 import { Request, Response, NextFunction } from 'express';
+import UserService from '../service/UserService';
 import JwtService from '../service/JwtService';
 
 export default class JwtMiddleware {
   public service;
 
+  public userService;
+
   constructor() {
     this.service = new JwtService();
+    this.userService = new UserService();
   }
 
   public generateToken = (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { username, classe, level, password } = req.body;
-      const token = this.service.genToken({ username, classe, level, password });
+      const { username, password } = req.body;
+      const token = this.service.genToken({ username, password });
       req.body.token = token;
       next();
     } catch (e) {
       console.log(e);
       return res.status(500).json({ message: 'Algo deu errado' });
+    }
+  };
+
+  public validateToken = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { authorization } = req.headers;
+      if (!authorization) {
+        return res.status(401).json({ error: 'Token not found' });
+      }
+      const user = this.service.validateToken(authorization);
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid token' });
+      }
+      req.body.username = user.username;
+      
+      next();
+    } catch (e) {
+      console.log(e);
+      next(e);
     }
   };
 }
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,9 +1,11 @@
 import userSchema from './userSchema';
 import productSchema from './productSchema';
 import loginSchema from './loginSchema';
+import arrayOfProductsSchema from './arrayOfProductsSchema';
 
 export {
   userSchema,
   productSchema,
   loginSchema,
+  arrayOfProductsSchema,
 };
